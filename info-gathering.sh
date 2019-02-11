6623fe027c3a97302e1f3a488d7788791ac721bf
RafaelSantos025 <45268280+RafaelSantos025@users.noreply.github.com>
2019-02-10 22:04:16 -0200

    Update info-gathering.sh

--- info-gathering.sh
+++ info-gathering.sh
@@ -5,14 +5,14 @@ then
 echo "ROOT Permission is necessary"
 exit 0
 else
-echo "∗ User Root !"
+echo "∗ Root ✔"
 fi
 
 DIR=wordlists/
 FILE=subdomains.txt
 if [ -e "$DIR$FILE" ]
 then
-echo "∗ Wordlist Exist !"
+echo "∗ Wordlist Exist ✔"
 else
 echo "!!! Missing wordlists/subdomains.txt !!!"
 exit 0
@@ -31,27 +31,27 @@ read dominio
 done
 
 dig all $dominio > $output/dig.txt
-echo "∗ DIG ⊕"
+echo "∗ DIG ✔"
 
 whois $dominio > $output/whois.txt
-echo "∗ WHOIS ⊕"
+echo "∗ WHOIS ✔"
 
 echo "Detectando WAF ..."
 wafw00f https://$dominio > $output/waf.txt
 echo "<------------------------------------------->" >> $output/waf.txt
 sqlmap --identify-waf -u $dominio --answers=follow=y >> $output/waf.txt
-echo "∗ WAF ⊕"
+echo "∗ WAF ✔"
 
 echo "Realizando Brute-Force de Subdominios ..."
 for word in $( cat wordlists/subdomains.txt )
 do
 host $word.$dominio >> $output/subdomains.txt
 done
-echo "∗ SUBDOMAINS ⊕"
+echo "∗ SUBDOMAINS ✔"
 
 echo "Realizando reconhecimento de portas e servicos ... (pode demorar)"
 nmap -sS -sV $dominio > $output/nmap.txt
-echo "∗ NMAP ⊕"
+echo "∗ NMAP ✔"
 
 echo "[!] Information gathering complete [!]"
 echo "[*] Arquivos salvos no Dir-Output [*]"
